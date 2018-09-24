acdc58e4029e0d96ff580674356dd690df47df4c
Walton <mtw0050@lx685v8n2-ee.unt.ad.unt.edu>
2018-09-24 13:27:21 -0500

    made hella fancy server/client :penguin:

--- socket_stuff/client.py
+++ socket_stuff/client.py
@@ -1,15 +1,83 @@
 import socket
-import sys
 import json
+import time
+from dronekit import Vehicle, connect
+import select
+import sys
+class Drone(Vehicle):
 
-file = open("sample.json").read()
-file = json.loads(file)
-IP_ADDRESS = "172.27.0.2"
-PORT = 5005
-LOCATION = (IP_ADDRESS,PORT)
+    def __init__(self, *args):
 
-print("TARGET IP: ", IP_ADDRESS)
-print("TARGET PORT: ", PORT)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(LOCATION)
-sock.sendall(json.dumps(file).encode("utf-8"))
+        super(Drone, self).__init__(*args)
+        self.sendDict = {}
+
+    def toJSON(self):
+        return json.dumps(self.sendDict, default=lambda o: o.__dict__)
+
+    def updateDict(self):
+
+        self.sendDict = {"global_loc": self.location.global_frame}
+
+class DummyDrone():
+
+    def __init__(self):
+        self.sendDict = {"dummy" : "data", "its" : "all fake"}
+
+class Client():
+
+    def __init__(self, HOST, PORT, type, name):
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.HOST = HOST
+        self.PORT = PORT
+        self.initMsgFrmClient = {"type" : type, "name" : name}
+        self.status = "default"
+        self.updateRate = 5
+        self.sending = True
+
+
+    def initVehicle(self):
+        try:
+            self.uav = connect('/dev/ttyACM0', wait_ready=True, vehicle_class=Drone)
+        except:
+            self.uav = DummyDrone()
+
+    def initConn(self):
+
+        self.sock.connect((self.HOST, self.PORT))
+        self.sock.sendall(json.dumps(self.initMsgFrmClient).encode("utf-8"))
+
+        data = self.sock.recv(1024)
+        _data = json.loads(data.decode("utf-8"))
+        self.updateRate = _data["freq"]
+        if _data["mode"] == "default":
+            self.sendData()
+
+    def sendData(self):
+
+        while True:
+
+            tic = time.time()
+            self.sock.sendall(json.dumps(self.uav.sendDict).encode("utf-8"))
+            r, _, _ = select.select([self.sock],[],[], .05)
+            if r:
+                data = self.sock.recv(1024)
+                _data = json.loads(data.decode("utf-8"))
+                if _data == '0':
+                    print("business as usual")
+            toc=  time.time() - tic
+            print(toc)
+            time.sleep(1/self.updateRate - toc)
+
+
+
+
+
+
+if __name__=="__main__":
+
+    HOST = '192.168.254.11'
+    PORT = 65432
+    node = Client(HOST,PORT,"UAV","rapunzel")
+    node.initVehicle()
+    node.initConn()
--- socket_stuff/server.py
+++ socket_stuff/server.py
@@ -1,26 +1,74 @@
+
 import socket
 import json
-import sys
-from io import StringIO
 
-def neat_data(data):
-    data = json.loads(data.decode("utf-8"))
-    with open('received.json', 'w') as outfile:
-        json.dump(data,outfile, indent=4, sort_keys=True)
-    print("dumppppppppppppppppp")
 
-IP_ADDRESS = "172.27.0.2"
-PORT = 5005
-LOCATION = (IP_ADDRESS,PORT)
+class VehicleObj:
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(LOCATION)
-while True:
-    sock.listen(1)
-    s, addr = sock.accept()
-    while True:
-        data = s.recv(1024)
-        if not data:
-            break
-        neat_data(data)
-    s.close()
+    def __init__(self, name, ip, vehicleType, connObj):
+        self.name = name
+        self.ip = ip
+        self.vehicleType = vehicleType
+        self.mode = "default"''
+        self.connObj = connObj
+
+class Server:
+
+    def __init__(self, HOST, PORT):
+        self.HOST = HOST
+        self.PORT = PORT
+        self.agents = {}
+        self.ipDict = {}
+        self.initMsgFrmSrv = {"mode" : "default", "freq" : 5}
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((self.HOST, self.PORT))
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.listen(5)
+
+    def initializeNode(self, conn, addr):
+
+        data = conn.recv(1024)
+        if data:
+            _data = json.loads(data.decode("utf-8"))
+            #dict with ip addresses and the name of the agent
+            self.ipDict[addr[0]] = _data["name"]
+            #agent dict with names as keys and a vehicle class as value
+            self.agents[_data["name"]] = (VehicleObj(_data["name"], addr[0], _data["type"], conn))
+            conn.sendall(json.dumps(self.initMsgFrmSrv).encode("utf-8"))
+
+    def parseData(self, conn, addr):
+
+        data = conn.recv(1024)
+        if data:
+            _data = json.loads(data.decode("utf-8"))
+            if "close connection" in _data:
+                del self.ipDict[addr[0]]
+            else:
+                #pass connection and vehicle object to the reply function
+                self.replyMsg(conn, self.agents[self.ipDict[addr[0]]])
+                pass
+
+
+    def listen(self):
+        self.conn, addr = self.sock.accept()  # this may need to get moved later when we switch to threading
+        while True:
+            if addr[0] in self.ipDict:
+                self.parseData(self.conn, addr)
+            else:
+                self.initializeNode(self.conn, addr)
+
+    def replyMsg(self, conn, agent):
+        #send back a 0 if there is nothing to change
+        print("in reply func")
+        if agent.mode == "default":
+            print("replying")
+            conn.sendall(json.dumps('0').encode("utf-8"))
+
+
+if __name__=="__main__":
+
+    HOST = '192.168.254.11'
+    PORT = 65432
+    queenB = Server(HOST, PORT)
+
+    queenB.listen()
