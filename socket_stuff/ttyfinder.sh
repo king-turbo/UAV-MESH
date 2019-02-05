05052a9eb946c1b381174fb53da271ac7f712d4b
king-turbo <mwalty@gmail.com>
2019-02-05 09:45:54 -0600

    writes to text file now

--- socket_stuff/ttyfinder.sh
+++ socket_stuff/ttyfinder.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
 
+rm "sysdisc.txt"
 for VARIABLE in 0 1 2 3 4 5
 do
 
     if udevadm info --query=all --name=/dev/ttyACM$VARIABLE | grep --q "Arduino_Mega"; then
-    echo "/dev/ttyACM""$VARIABLE"
+    touch "sysdisc.txt"
+    echo "/dev/ttyACM""$VARIABLE" >> "sysdisc.txt"
+    echo "ardupilot" >> "sysdisc.txt"
     fi
 done
 
